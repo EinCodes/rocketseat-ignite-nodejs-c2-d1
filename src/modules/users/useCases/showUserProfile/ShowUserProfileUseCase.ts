7fce9237d5c28f6265aaaa31a86d007a9c87a502
EinCodes <ein.xmi@gmail.com>
2022-06-19 16:52:16 -0300

    (Step 3.5) Implementar o caso de uso `ShowUserProfileUseCase`

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -6,11 +6,16 @@ interface IRequest {
 }
 
 class ShowUserProfileUseCase {
-  constructor(private usersRepository: IUsersRepository) { }
+  constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User | void {
-    // Remover Void
-    // Complete aqui
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(`User with id ${user_id} not found`);
+    }
+
+    return user;
   }
 }
 
