a96a2c15e33399f1da956804567ca985948ca6c0
EinCodes <ein.xmi@gmail.com>
2022-06-19 17:36:04 -0300

    (Step 3.7) Ajustar os testes da rota GET `/users`

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from "express";
-
+import { IncomingHttpHeaders } from "http";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
-class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
+interface ICustomHeaders {
+  user_id: string;
+}
 
-  handle(request: Request, response: Response): Response | void {
-    // Remover Void
-    // Complete aqui
+interface IReqCustom<THeader> extends Request {
+  headers: IncomingHttpHeaders & THeader;
+}
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: IReqCustom<ICustomHeaders>, response: Response): Response {
+    try {
+      const { user_id } = request.headers;
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({
+        error: error.message,
+      });
+    }
   }
 }
 
