fe9ec0a7bee4a464cc74b07d3495b0021bccd43a
EinCodes <ein.xmi@gmail.com>
2022-06-19 16:54:22 -0300

    (Step 3.6) Implementar o caso de uso `TurnUserAdminUseCase`

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -8,9 +8,14 @@ interface IRequest {
 class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User | void {
-    // Remover Void
-    // Complete aqui
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(`User with id ${user_id} not found`);
+    }
+
+    return this.usersRepository.turnAdmin(user);
   }
 }
 
