dd3ad8fbdfb4a79727d9e29ca19d44cd4b7b9fcd
EinCodes <ein.xmi@gmail.com>
2022-06-19 16:51:59 -0300

    (Step 3.4) Implementar o caso de uso `ListAllUsersUseCase`

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,11 +6,20 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) { }
+  constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] | void {
-    // Remover Void
-    // Complete aqui
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(`User with id ${user_id} not found`);
+    }
+
+    if (!user.admin) {
+      throw new Error(`Current user has no admin privileges`);
+    }
+
+    return this.usersRepository.list();
   }
 }
 
